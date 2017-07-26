396493d30131b74bc4b5545db4cd258b45a0eeab
Siva Kumar Gopi <sivakumar1592@gmail.com>
2017-07-26 16:21:18 -0400

    updated for Incarnation 1 and 2

--- Final integrated code for Incarnation1.py
+++ Final integrated code for Incarnation1.py
@@ -1,4 +1,8 @@
+
+
 from pip._vendor.distlib.compat import raw_input
+
+
 '''
 findPIByNilakantaInfiniteSeries
 
@@ -10,22 +14,26 @@ The series is:
 3 + 4/(4*5*6) - 4/(4*5*6) + 4/(6*7*8) - 4/(8*9*10) + ...
 '''
 
-
 def findPIByNilakantaInfiniteSeries(terms):
-    # base case
+    # Base case
     if terms == 0:
         return 3
     else:
         seriesValue = 1
         value = 4.0 / ((terms * 2) * ((terms * 2) + 1) * ((terms * 2) + 2))
         if terms % 2 == 0:
-            # negative value
+            # Negative value
             seriesValue = seriesValue * -1 * value
         else:
-            # positive value
+            # Positive value
             seriesValue = seriesValue * value
+
+        # Recursive function
         return seriesValue + findPIByNilakantaInfiniteSeries(terms - 1)
 
+'''
+Function that returns the PI value
+'''
 
 def PI():
     return findPIByNilakantaInfiniteSeries(500)
@@ -35,8 +43,9 @@ def PI():
 Converts degrees to radians
 radians = (degress/180) * pi
 '''
+
 def findRadians(degrees):
-    # Find the radians for the number
+    # Find the radians for the degrees.
     return (degrees / 180) * PI()
 
 
@@ -45,35 +54,39 @@ Find the power of X
 
 E.g: x^4 = x*x*x*x
 '''
+
 def computePowerOf(radians, terms):
     # base case
     if terms == 0:
         return 1
     else:
+        # Recursive function
         return radians * computePowerOf(radians, terms-1)
+
 
 '''
 Find the factorial of X
 
 E.g: 5! = 5*4*3*2*1
 '''
+
 def factorialOf(number):
     # base case
     if number == 1 or number == 0:
         return 1
     else:
+        # Recursive function
         return number * factorialOf(number-1)
 
 
-
 '''
-Function to find the sine using taylor series
+Function to find the sin using taylor series
 
 sin(x) = x- x^3/3! + x^5/5! - x^7/7! + ...
-
-
-To get accurate values upto 12 digits, the infinite series is called for 85 times.
+To get accurate values up to 12 digits, the infinite series is called
+for 80 times.
 '''
+
 def taylorSeriesForSine(radians, terms):
     # base case
     if terms == 0:
@@ -87,17 +100,26 @@ def taylorSeriesForSine(radians, terms):
         else:
             # negative value
             seriesValue = seriesValue * -1 * (value / factorialOf((2*terms)+1))
+
+    # Recursive function
     return seriesValue + taylorSeriesForSine(radians, terms-1)
+
+'''
+Function that calculates the sine value in alpha function
+'''
 
 def Sine(radians):
     return taylorSeriesForSine(radians, 80)
+
 
 '''Function to find the cosine using taylor series
 
 cos(x) = 1 - x^2/2! + x^4/4! - x^6/6! + ...
 
-To get accurate values upto 12 digits, the infinite series is called for 85 times.
+To get accurate values up to 12 digits, the infinite series is called for
+80 times.
 '''
+
 def taylorSeriesForCosine(radians, terms):
     # base case
     if terms == 0:
@@ -116,42 +138,63 @@ def taylorSeriesForCosine(radians, terms):
 '''
 Function that calculates the cosine value for alpha/2
 '''
+
 def Cosine(degrees):
-    #print "the degree is", degrees
+
     radians = findRadians(degrees)
     return taylorSeriesForCosine(radians, 80)
 
 
 '''
-Function to calculate the aplha value using Bisection method
+Function to calculate the alpha value using Bisection method
+No.of iterations>logbase2((lowerLimit-upperLimt)/error tolerance level)
 '''
+
 def alphaValue(iterations):
+
+    # LowerLimit and upperLimit values are based on assumptions.
     lowerLimit=0
     upperLimit=PI()
     baseValue=1
+
     while(baseValue<iterations):
         midPoint=(lowerLimit+upperLimit)/2
+
+        #f(x)=x-sin(x)-pi/2
         valueofLowerlimit=lowerLimit-Sine(lowerLimit)-(PI()/2)
         valueofmidPoint=midPoint-Sine(midPoint)-(PI()/2)
-        if ((valueofLowerlimit>0 and valueofmidPoint<0) or (valueofLowerlimit<0 and valueofmidPoint>0)):
-                upperLimit=midPoint
-                baseValue=baseValue+1
+
+        # Onelimit should be in positive range and other in negative range.
+        if ((valueofLowerlimit>0 and valueofmidPoint<0)
+        or (valueofLowerlimit<0 and valueofmidPoint>0)):
+
+                upperLimit=midPoint  # Manipulating limits for next iteration
+                baseValue=baseValue+1  # Increment baseValue
         else:
-                lowerLimit=midPoint
-                baseValue=baseValue+1
+                lowerLimit=midPoint  # Manipulating limits for next iteration
+                baseValue=baseValue+1  # Increment baseValue
 
     return ((lowerLimit+upperLimit)/2)
 
+
+
 def main():
+
+    # Accepting user input.
     inputValue = raw_input("Enter the radius of the circle: ")
 
     try:
         radiusOfCircle = float(inputValue)
-        #print "The alphavalue is", alphaValue(9)
-        alpha = alphaValue(9)
+
+        # Call alpha function
+        alpha = alphaValue(35)
+        print ("The alphavalue is"), alpha
+        print ("cos(alpha/2) value is"), Cosine(alpha/2)
         # length of the segment = 2R(1- cos(alpha/2))
         lengthOfSegment = 2 * radiusOfCircle * (1.0 - Cosine(alpha/2))
         print ("The length of the segment is"), lengthOfSegment
+
+    # Handling exceptions.
     except ValueError:
         print ("Please enter an integer or float")
     except OverflowError:
--- Incarnation2.py
+++ Incarnation2.py
@@ -2,26 +2,40 @@
 
 from pip._vendor.distlib.compat import raw_input
 import math
-import xml.etree.ElementTree as ET
+import xml.etree.ElementTree as ET  # For creating XML
 import ctypes
+import pdb  # For debugging
+import logging  # For log file
+import sys
 
 '''
-Function to calculate the aplha value using Bisection method
+Function to calculate the alpha value using Bisection method
+No.of iterations>logbase2((lowerLimit-upperLimt)/error tolerance level)
 '''
 def alphaValue(iterations):
+
+    # LowerLimit and upperLimit values are based on assumptions.
     lowerLimit=0
     upperLimit=math.pi
     baseValue=1
+
     while(baseValue<iterations):
         midPoint=(lowerLimit+upperLimit)/2
+
+        #f(x)=x-sin(x)-pi/2
         valueofLowerlimit=lowerLimit-math.sin(lowerLimit)-(math.pi/2)
         valueofmidPoint=midPoint-math.sin(midPoint)-(math.pi/2)
-        if ((valueofLowerlimit>0 and valueofmidPoint<0) or (valueofLowerlimit<0 and valueofmidPoint>0)):
-                upperLimit=midPoint
-                baseValue=baseValue+1
+
+        # Onelimit should be in positive range and other in negative range.
+        if ((valueofLowerlimit>0 and valueofmidPoint<0)
+        or (valueofLowerlimit<0 and valueofmidPoint>0)):
+
+                upperLimit=midPoint  # Manipulating limits for next iteration
+                baseValue=baseValue+1  # Increment baseValue
         else:
-                lowerLimit=midPoint
-                baseValue=baseValue+1
+
+                lowerLimit=midPoint  # Manipulating limits for next iteration
+                baseValue=baseValue+1  # Increment baseValue
 
     return ((lowerLimit+upperLimit)/2)
 
@@ -30,30 +44,43 @@ Function to calculate the length of the segment X1X2
 '''
 
 def lengthOfSegment(radiusOfCircle):
-      alpha = math.radians(alphaValue(9))
-      # length of the segment = 2R(1- cos(alpha/2))
+      #calculating alpha value in terms of radians.
+      alpha = math.radians(alphaValue(35))
+      print (" The alpha value is"),alphaValue(35)
+      # length of the segment = 2R(1- cos(alpha/2)).
       lengthOfSegment = 2 * radiusOfCircle * (1.0 - math.cos(alpha/2))
 
       return lengthOfSegment
 
 def main():
+
+    # Accepting user input.
     inputValue = raw_input("Enter the radius of the circle: ")
 
     try:
         radiusOfCircle = float(inputValue)
+        #LOG_FILENAME = 'example.log'
+        #logging.basicConfig(filename=LOG_FILENAME, level=logging.DEBUG)
+
+        pdb.set_trace()  # Set trace point
         length=lengthOfSegment(radiusOfCircle)
+
+        #creating XML.
         root = ET.Element('Output')
         root.text = str(length)
         ET.dump(root)
         tree= ET.ElementTree(root)
-        tree.write('Output.xml')
-        #print ("The length of the segment is"), length
+        tree.write('Output.xml')  # Writing to an XML file
 
-        tree = ET.parse('Output.xml')
+
+        tree = ET.parse('Output.xml')  # Reading from an XML file
         root = tree.getroot()
-        return ctypes.windll.user32.MessageBoxA(None, root.text, "Length of the segment", 0)
 
-
+        # GUI to print length of the segment.
+        return ctypes.windll.user32.MessageBoxA(None,
+                                                root.text,
+                                                "Length of the segment", 0)
+    # Handling exceptions.
     except ValueError:
         print ("Please enter an integer or float")
     except OverflowError:
